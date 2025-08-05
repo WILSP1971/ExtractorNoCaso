2e7fc8863f9acc6d5a44a0be740c52eef6697029
WILSONSP1971 <wilson.sp03@gmail.com>
2025-08-05 16:20:18 -0500

    subiendo a github

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 import pytesseract
 
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'bmp', 'tiff'}
